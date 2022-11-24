4d4c0b01407c3abed2d7667d6ea1552fc067fe33
BautiFelli <82039134+BautiFelli@users.noreply.github.com>
2022-11-24 16:17:39 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,34 @@
+function abrirEtiquetaNO(){
+    var overlay= document.getElementById('etiNo');
+    overlay.classList.remove('no-mostrar');
+  }
+
+  function abrirEtiquetaSI(){
+    var overlay= document.getElementById('etiSi');
+    overlay.classList.remove('no-mostrar');
+  }
+
+  function abrirPopUpDefo(){
+    var popUp= document.getElementById("popDefo");
+    popUp.classList.remove("no-mostrar");
+  }
+
+function abrirPopupFrutos(){
+    var popFrutos=document.getElementById("frutosPop");
+    popFrutos.classList.remove("no-mostrar");
+}
+
+function abrirPopCN(){
+  var popNormal=document.getElementById("cafeNormal");
+  popNormal.classList.remove("no-mostrar");
+}
+
+function abrirPopCJ(){
+  var popNormal=document.getElementById("cafeCJ");
+  popNormal.classList.remove("no-mostrar");
+}
+
+function abrirPopOrg(){
+  var popOrg= document.getElementById("popOrg");
+  popOrg.classList.remove("no-mostrar");
+}
